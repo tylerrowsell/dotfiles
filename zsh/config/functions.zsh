dd7693a70f0d7de5c4e3aaaf14813d498dd15481
Tyler Rowsell <tyler.rowsell@shopify.com>
2021-05-06 09:15:24 -0400

    add-boop-function

--- zsh/config/functions.zsh
+++ zsh/config/functions.zsh
@@ -8,3 +8,14 @@ spinup() {
         spin create online-store-web
     fi
 }
+
+boop() {
+    git add .
+    if [ -n "$1" ]
+    then
+        git commit -m "$1"
+    else
+        git commit -m "boop"
+    fi
+    git pushf
+}
